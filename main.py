eed0c0dc376b54777efe4b1219f47fd1260222b2
michaelnicolosi <mike@mikenicolosi.com>
2018-10-09 15:35:17 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib2
 
 class HourCronPage(webapp2.RequestHandler):
     def get(self):
-        request = urllib2.Request('<GCF URL>', headers={"cronrequest" : "true"})
+        request = urllib2.Request('https://us-central1-gcp-test-193900.cloudfunctions.net/function-1', headers={"cronrequest" : "true"})
         contents = urllib2.urlopen(request).read()
 
 app = webapp2.WSGIApplication([
